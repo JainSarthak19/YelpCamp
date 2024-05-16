7fdf296c1989871c4b553bc81bbb5f2fbd4493f7
Sarthak Jain <jainsarthak.1414@gmail.com>
2024-05-16 14:18:18 +0530

    removed bugs

--- seeds/index.js
+++ seeds/index.js
@@ -21,13 +21,13 @@ async function main() {
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({}, { timeout: 20000 });
+    await Campground.deleteMany({});
     for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
 
         const camp = new Campground({
-            author: '663b675b1f8df58f1441e03a',
+            author: '664398a40241005aca5340a8',
             location: `${cities[random1000
             ].city},${cities[random1000
             ].state}`,
